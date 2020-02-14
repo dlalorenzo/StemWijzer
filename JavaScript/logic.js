0f91e01c6978e56e77980339104dc1f59a140c41
lorenzo <99051451@mydavinci.nl>
2020-02-14 14:23:01 +0100

    front end js
    ben begonnen aan de javascript en heb alles ge linked enzo

---
+++ JavaScript/logic.js
@@ -0,0 +1,12 @@
+console.log(parties);
+console.log(subjects);
+
+console.log (parties[0].name);
+
+var ding = parties[0].name;
+
+function test() {
+  document.getElementById("demo").innerHTML = "MISTER BLING";
+}
+
+test();
